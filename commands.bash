e3ac43ed231ea0df8212e070bffef87888da57e7
chezzoba <58479296+chezzoba@users.noreply.github.com>
2021-01-02 07:28:54 +0100

    Created Template

---
+++ commands.bash
@@ -0,0 +1,9 @@
+aws s3 mb s3://alexaliftskill/
+
+sam package --s3-bucket alexaliftskill --template-file template.yaml --output-template-file gen/cloudformationtemplate.yaml
+
+sam deploy --template-file gen/cloudformationtemplate.yaml --stack-name AlexaLiftSkill --region us-east-1 --capabilities CAPABILITY_IAM
+
+
+aws s3 cp src/src.zip s3://alexaliftskill/source.zip;
+aws lambda update-function-code --function-name AlexaLiftSkill-AlexaFunction-1UOTFQ575R74N --s3-bucket alexaliftskill --s3-key source.zip
