f661b38cd4247dee5704681a5568bc5fb19e282f
chezzoba <kaizad@email.com>
2021-01-02 20:37:51 +0100

    Updated message

--- commands.bash
+++ commands.bash
@@ -4,6 +4,6 @@ sam package --s3-bucket alexaliftskill --template-file template.yaml --output-te
 
 sam deploy --template-file gen/cloudformationtemplate.yaml --stack-name AlexaLiftSkill --region us-east-1 --capabilities CAPABILITY_IAM
 
-
-aws s3 cp src/src.zip s3://alexaliftskill/source.zip
+cd src && zip -r ../src.zip * && cd .. && 
+aws s3 cp src.zip s3://alexaliftskill/source.zip && 
 aws lambda update-function-code --function-name AlexaLiftSkill-AlexaFunction-1UOTFQ575R74N --s3-bucket alexaliftskill --s3-key source.zip
--- src/app.js
+++ src/app.js
@@ -1,8 +1,132 @@
+
 const Alexa = require("ask-sdk-core");
 const axios = require("axios");
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
-const sns = new AWS.SNS({apiVersion: '2010-03-31'});  
+const sns = new AWS.SNS({ apiVersion: "2010-03-31" });
+const dynamodb = new AWS.DynamoDB.DocumentClient({ region: "us-east-1" });
+
+const getDay = () => {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const dayNumber = new Date();
+  return days[dayNumber.getDay()];
+};
+
+const day = getDay();
+
+const get = async (number) => {
+  const params = {
+    Key: { phone: number },
+    TableName: process.env.TABLE,
+    AttributesToGet: [day, "week"],
+  };
+
+  const lift = await dynamodb
+    .get(params, (err, data) => {
+      if (err) console.log(err, err.stack);
+      else return data;
+    })
+    .promise();
+
+  return lift.Item;
+};
+
+const put = async (number, liftName, trainingMax) => {
+  const entry = new Object();
+  const attr = liftName
+    .split(" ")
+    .map((w) => w[0].toUpperCase() + w.slice(1).toLowerCase());
+  entry[attr.join(" ")] = trainingMax;
+  const params = {
+    Key: { phone: number },
+    TableName: process.env.TABLE,
+    UpdateExpression: "set #a = :l, ",
+    ExpressionAttributeNames: { "#a": day },
+    ExpressionAttributeValues: { ":l": entry },
+    ReturnValues: "UPDATED_NEW",
+  };
+  const res = await dynamodb
+    .update(params, (err, data) => {
+      if (err) console.log(err);
+      else return data;
+    })
+    .promise();
+
+  return res.Attributes;
+};
+
+const updateWeek = async (number, neWeek) => {
+  const params = {
+    Key: { phone: number },
+    TableName: process.env.TABLE,
+    UpdateExpression: "set #a = :l, ",
+    ExpressionAttributeNames: { "#a": "week" },
+    ExpressionAttributeValues: { ":l": neWeek },
+    ReturnValues: "UPDATED_NEW",
+  };
+  const res = await dynamodb
+    .update(params, (err, data) => {
+      if (err) console.log(err);
+      else return data;
+    })
+    .promise();
+
+  return res.Attributes;
+};
+
+const getNumber = async ({ apiAccessToken, apiEndpoint }) => {
+  const { status, data } = await axios.get(
+    apiEndpoint + "/v2/accounts/~current/settings/Profile.mobileNumber",
+    { headers: { Authorization: "Bearer " + apiAccessToken } }
+  );
+  return status == 200 && data;
+};
+
+const processWeight = (week, dayObject, kilos = true) => {
+  const [lift, trainingMax] = Object.entries(dayObject)[0];
+  if (lift === "Rest") {
+    return ["Today is your Rest Day!"];
+  } else {
+    const basePercent = week === 1 ? 75 : week === 3 ? 70 : 65;
+    const cvrt = kilos ? 1 : 2.20462262185;
+    const unt = kilos ? 2.5 : 5;
+    const percentages = [
+      basePercent,
+      basePercent + 10,
+      basePercent + 20,
+      basePercent,
+    ];
+    const weights = percentages.map(
+      (percent) =>
+        Math.round((trainingMax * percent * cvrt) / (unt * 100)) * unt
+    );
+    const unit = kilos ? "kilos" : "pounds";
+    const msg = [
+      `Today you are doing the ${lift} for: `,
+      `${week === 1 ? "A" : week} rep${week === 1 ? "" : "s"} of ${
+        weights[0]
+      } ${unit}, `,
+      `${week === 1 ? "A" : week} rep${week === 1 ? "" : "s"} of ${
+        weights[1]
+      } ${unit}, `,
+      `More than ${week === 1 ? "a" : week} rep${week === 1 ? "" : "s"} of ${
+        weights[2]
+      } ${unit}, `,
+      `5 sets of 5 reps of ${weights[3]} ${unit}`,
+    ];
+    return msg;
+  }
+};
+
+const oneRepMax = (mass, reps) => Math.round(0.9 * mass * (1 + reps / 3));
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
@@ -11,7 +135,11 @@ const LaunchRequestHandler = {
     );
   },
   handle(handlerInput) {
-    const speakOutput = "Hi, say your option";
+    const today = new Date();
+    const time = today.getHours();
+    const greeting =
+      5 <= time < 12 ? "Morning" : 12 <= time < 17 ? "Afternoon" : "Evening";
+    const speakOutput = `Good ${greeting}. Ask or tell me how much you lifted.`;
 
     return handlerInput.responseBuilder
       .speak(speakOutput)
@@ -28,29 +156,49 @@ const PhoneMessageHandler = {
     );
   },
   async handle(handlerInput) {
-    const {
-      apiAccessToken,
-      apiEndpoint,
-    } = handlerInput.requestEnvelope.context.System;
     try {
-      const { status, data } = await axios.get(
-        apiEndpoint + "/v2/accounts/~current/settings/Profile.mobileNumber",
-        { headers: { "Authorization": "Bearer " + apiAccessToken } }
+      const { phoneNumber, countryCode } = await getNumber(
+        handlerInput.requestEnvelope.context.System
       );
-      speakOutput =
-        status == 200 && data
-          ? `Your Phone Number is: ${data.countryCode} ${data.phoneNumber}`
-          : "Hello There";
-    } catch (err) {
+      const number = `+${countryCode}${phoneNumber}`;
+      const data = await get(number);
+      if (!data) {
         return handlerInput.responseBuilder
-        .speak('You need to give me your number for this to work. Check your permissions')
+          .speak(
+            "You haven't told me anything yet. Believe me I'd remember if you did."
+          )
+          .getResponse();
+      }
+      const msg = processWeight(parseInt(data.week), data[day]);
+      const params = {
+        PhoneNumber: number,
+        Message: msg.join("\n"),
+        MessageAttributes: {
+          "AWS.SNS.SMS.SenderID": {
+            DataType: "String",
+            StringValue: "MyLifts",
+          }
+        },
+      };
+      await sns.publish(params, (err, data) => {
+        if (err) console.log(err);
+        else console.log(data);
+      }).promise();
+      return handlerInput.responseBuilder
+        .speak("Alright! I texted you!")
+        .reprompt('Is there something else I can help you with?')
+        .getResponse();
+    } catch (err) {
+      console.log(err);
+      if (err.message.slice(-3) in ['401', '403']) {
+        return handlerInput.responseBuilder
+        .speak(
+          "You need to give me your number for this to work. Check your permissions "
+        )
         .withAskForPermissionsConsentCard(["alexa::profile:mobile_number:read"])
         .getResponse();
+      }
     }
-    return handlerInput.responseBuilder
-        .speak('Alright! I texted you!')
-        .reprompt(speakOutput)
-        .getResponse();
   },
 };
 
@@ -62,7 +210,8 @@ const HelpIntentHandler = {
     );
   },
   handle(handlerInput) {
-    const speakOutput = "You can say hello to me! How can I help?";
+    const speakOutput =
+      "Ask me 'How much should I lift today?' or tell me how much you did today.";
 
     return handlerInput.responseBuilder
       .speak(speakOutput)
@@ -87,11 +236,24 @@ const CancelAndStopIntentHandler = {
     return handlerInput.responseBuilder.speak(speakOutput).getResponse();
   },
 };
-/* *
- * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
- * It must also be defined in the language model (if the locale supports it)
- * This handler can be safely added but will be ingnored in locales that do not support it yet
- * */
+
+const TellMeMyLiftIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === "MyLiftIntent"
+    );
+  },
+  handle(handlerInput) {
+    const speakOutput = "Goodbye!";
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(speakOutput)
+      .getResponse();
+  },
+};
+
 const FallbackIntentHandler = {
   canHandle(handlerInput) {
     return (
@@ -182,6 +344,7 @@ exports.handler = Alexa.SkillBuilders.custom()
   .addRequestHandlers(
     LaunchRequestHandler,
     PhoneMessageHandler,
+    TellMeMyLiftIntentHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     FallbackIntentHandler,
@@ -189,4 +352,5 @@ exports.handler = Alexa.SkillBuilders.custom()
     IntentReflectorHandler
   )
   .addErrorHandlers(ErrorHandler)
+  .withCustomUserAgent('LiftApp')
   .lambda();
