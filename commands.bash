6296442e631a56d7877579ad3bcb6026ee097f3d
chezzoba <58479296+chezzoba@users.noreply.github.com>
2021-01-02 11:54:00 +0100

    Wrote phone number

--- commands.bash
+++ commands.bash
@@ -5,5 +5,5 @@ sam package --s3-bucket alexaliftskill --template-file template.yaml --output-te
 sam deploy --template-file gen/cloudformationtemplate.yaml --stack-name AlexaLiftSkill --region us-east-1 --capabilities CAPABILITY_IAM
 
 
-aws s3 cp src/src.zip s3://alexaliftskill/source.zip;
+aws s3 cp src/src.zip s3://alexaliftskill/source.zip
 aws lambda update-function-code --function-name AlexaLiftSkill-AlexaFunction-1UOTFQ575R74N --s3-bucket alexaliftskill --s3-key source.zip
